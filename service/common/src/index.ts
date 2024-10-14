38fdfd3d6df3e85622c7aba2810b92a67de13c73
Kota <yuzukh0211@gmail.com>
2024-10-14 23:41:30 +0900

    fix:message

--- service/common/src/index.ts
+++ service/common/src/index.ts
@@ -1,3 +1,4 @@
 export const INVALID_END_DATE = "釣行終了が釣行開始よりも過去の日付です";
 export const INVALID_START_DATE = "釣行開始が過去の日付です";
-export const INVALID_APPLYING_END_DATE = "申し込み期限が過去の日付です";
+export const INVALID_APPLYING_END_DATE =
+  "申し込み期限が過去の日付/釣行開始日よりも未来の日付です";
