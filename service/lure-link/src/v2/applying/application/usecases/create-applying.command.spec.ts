9468180962822598436cc034a0d45c370dcd5568
Kota <yuzukh0211@gmail.com>
2024-10-21 23:24:27 +0900

    [Red] usecase for create

---
+++ service/lure-link/src/v2/applying/application/usecases/create-applying.command.spec.ts
@@ -0,0 +1,65 @@
+import { Test } from '@nestjs/testing';
+
+import { createMock } from '@golevelup/ts-jest';
+import dayjs from 'src/lib/dayjs';
+import { CreateApplyingCommandHandler } from './create-applying.command';
+import {
+  APPLYING_REPOSITORY_TOKEN,
+  IApplyingRepository,
+} from '../../domain/applying.repository';
+
+describe('CreateApplyingCommnadHandler', () => {
+  let validCommand;
+  let commandHandler: CreateApplyingCommandHandler;
+  const mock = createMock<IApplyingRepository>();
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        {
+          provide: APPLYING_REPOSITORY_TOKEN,
+          useValue: mock,
+        },
+        CreateApplyingCommandHandler,
+      ],
+    }).compile();
+    commandHandler = module.get<CreateApplyingCommandHandler>(
+      CreateApplyingCommandHandler,
+    );
+    mock.save.mockResolvedValue();
+    validCommand = {
+      userId: '01JA071A96K19YQJVKV5FDBW6X',
+      firstPickUpOption: {
+        prefecture: '神奈川県',
+        address: '茅ヶ崎市1-13-31',
+        description: '江ノ島です',
+      },
+      secondPickUpOption: {
+        prefecture: '埼玉県',
+        address: '川越市1-24-14',
+        description: 'ファミリーマート〇〇店前です',
+      },
+      thirdPickUpOption: {
+        prefecture: '埼玉県',
+        address: '川越市1-24-14',
+        description: 'ファミリーマート〇〇店前です',
+      },
+    };
+  });
+
+  describe('valid repository', () => {
+    it('valid case', async () => {
+      const actual = await commandHandler.execute(validCommand);
+      expect(actual).toEqual(expect.any(String));
+    });
+  });
+  describe('invalid repostory', () => {
+    it('occurs error if save failed', async () => {
+      const expectedErr = new Error('UT');
+      mock.save.mockReset();
+      mock.save.mockRejectedValue(expectedErr);
+      await expect(async () =>
+        commandHandler.execute(validCommand),
+      ).rejects.toThrow(Error);
+    });
+  });
+});
---
+++ service/lure-link/src/v2/applying/application/usecases/create-applying.command.ts
@@ -0,0 +1,31 @@
+import { CommandHandler, ICommand, ICommandHandler } from '@nestjs/cqrs';
+
+import { Inject } from '@nestjs/common';
+import dayjs from 'src/lib/dayjs';
+import { Place } from 'src/v2/recruitment/domain/events/recruitment-created-event';
+import {
+  APPLYING_REPOSITORY_TOKEN,
+  IApplyingRepository,
+} from '../../domain/applying.repository';
+
+export class CreateApplyingCommand implements ICommand {
+  recruitmentId: string;
+  userId: string;
+  firstPickUpOption: Place;
+  secondPickUpOption?: Place | undefined;
+  thirdPickUpOption?: Place | undefined;
+}
+
+@CommandHandler(CreateApplyingCommand)
+export class CreateApplyingCommandHandler
+  implements ICommandHandler<CreateApplyingCommand>
+{
+  constructor(
+    @Inject(APPLYING_REPOSITORY_TOKEN)
+    private readonly repo: IApplyingRepository,
+  ) {}
+  async execute(command: CreateApplyingCommand): Promise<string> {
+    const currentDate = dayjs().toISOString();
+    throw new Error();
+  }
+}
