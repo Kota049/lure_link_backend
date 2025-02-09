960c44cf1fecdd7d6b1ce905f062037f0e92873b
Kota <yuzukh0211@gmail.com>
2025-02-09 16:55:15 +0900

    add:cors

--- service/lure-link/src/main.ts
+++ service/lure-link/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 開発用に一時的に付与
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
   await app.listen(4000);
 }
 bootstrap();
